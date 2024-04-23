cbbac8b875993f88a199b66e02298033564b03c0
Jpalomo <Jpalomo92@gmail.com>
2024-04-22 22:39:18 -0500

    added the directories for the app to use

--- server/server.js
+++ server/server.js
@@ -29,11 +29,17 @@ const startApolloServer = async () => {
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.use(routes);
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+})}
+
+
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+};
+
+startApolloServer();
